213a341556b114f3fef9b974111cfa5d5973ba92
MarioGafeed <jesushostingme@gmail.com>
2024-02-08 16:23:17 +0200

    remove cart

--- core/resources/views/front/construction/layout.blade.php
+++ core/resources/views/front/construction/layout.blade.php
@@ -225,21 +225,7 @@
         @endif
     </footer><!-- End finlance_footer section -->
 
-    @if ($bex->is_shop == 1 && $bex->catalog_mode == 0)
-        <div id="cartIconWrapper">
-            <a class="d-block" id="cartIcon" href="{{ route('front.cart') }}">
-                <div class="cart-length">
-                    <i class="fas fa-cart-plus"></i>
-                    <span class="length">{{ cartLength() }} {{ __('ITEMS') }}</span>
-                </div>
-                <div class="cart-total">
-                    {{ $bex->base_currency_symbol_position == 'left' ? $bex->base_currency_symbol : '' }}
-                    {{ cartTotal() }}
-                    {{ $bex->base_currency_symbol_position == 'right' ? $bex->base_currency_symbol : '' }}
-                </div>
-            </a>
-        </div>
-    @endif
+    
 
     <!--====== PRELOADER PART START ======-->
     @if ($bex->preloader_status == 1)
